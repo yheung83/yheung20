e1e1f997a75ed9224ff643d7c8c35a9130096268
yheung20 <92213295+yheung20@users.noreply.github.com>
2022-05-06 09:17:52 +0900

    Update rsi-final.py

--- rsi-final.py
+++ rsi-final.py
@@ -74,11 +74,12 @@ while(True):
     def buy(coinlist): 
         avg_buy_price = upbit.get_avg_buy_price('KRW-ZRX')
         krw_balance = upbit.get_balance("KRW")
-        krw_price = 102980
+        krw_price1 = 102980
+        krw_price2 = krw_balance / 4
         if krw_balance > krw_price and avg_buy_price == 0: 
-            upbit.buy_market_order(ticker=coinlist, price=9000000, )
+            upbit.buy_market_order(ticker=coinlist, price=krw_price2, )
         if krw_balance > krw_price and avg_buy_price > 0:
-            upbit.buy_market_order(ticker=coinlist, price=krw_price, )
+            upbit.buy_market_order(ticker=coinlist, price=krw_price1, )
         else:
             pass
         return
